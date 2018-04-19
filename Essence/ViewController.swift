8c3570db840ea0def8c378a551db96cb110f29d1
SMT <bharath@smtindia.net>
2018-04-19 14:56:29 +0530

    Initial Commit

---
+++ Essence/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Essence
+//
+//  Created by SmartMobile Technologies on 19/04/18.
+//  Copyright © 2018 SmartMobile Technologies. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        //first commit
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
